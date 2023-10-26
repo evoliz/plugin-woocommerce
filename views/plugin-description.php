d5721ae61544c3fd6dc6d63ab3585e998d5bdbba
Florent Dehanne <florent.dehanne@gmail.com>
2023-10-26 17:12:38 +0200

    Display required configuration

---
+++ views/plugin-description.php
@@ -0,0 +1,9 @@
+<p>La connexion du module Evoliz enrichit votre expérience WooCommerce. A compter de l’installation du plugin, bénéficiez de :</p>
+<p><b>La synchronisation des nouveaux clients : </b>les clients qui effectuent une commande sont créés dans Evoliz en temps réel. Grâce aux champs [Société/Nom] et/ou [Nom du contact], nous pouvons analyser si le client est existant dans Evoliz, et ce, afin de ne pas créer de doublon.</p>
+<p><b>La synchronisation des nouveaux contacts clients : </b>les contacts clients associés au client sont également créés dans Evoliz. Là aussi, les champs [Email] et [ID du client associé] nous permettent de  détecter les contacts clients existants afin de ne pas créer de doublon.</p>
+<p>
+    <b>La synchronisation des opérations de ventes : </b>
+    <br/>- toute <b>commande à l’état « en cours »</b> génère la création d’un bon de commande dans Evoliz
+    <br/>- toute <b>commande à l’état « terminée »</b> génère la création d’une facture dans Evoliz ainsi que le paiement qu’il lui est associé
+</p>
+<p><b>Bon à savoir : </b>nous avons fait le choix de NE PAS synchroniser tout l'historique WooCommerce (clients, contacts client…). Seules les nouvelles commandes généreront les différentes synchronisations présentées ci-dessus.</p>
---
+++ views/required-configuration.php
@@ -0,0 +1,11 @@
+<div class="evoliz-required-info">
+    <p>Pour réussir votre connexion Evoliz et WooCommerce, certains pré requis sont obligatoires à la fois concernant le <strong>profil de l'utilisateur</strong> ainsi que <strong>certains paramétrages au niveau du compte :</strong></p>
+
+    <ul>
+        <li>Les menus <strong>Ventes et Commandes</strong> doivent figurer dans le menu principal de l'utilisateur</li>
+        <li>Les profils avec droit utilisateur doivent pouvoir <strong>Exporter les données</strong></li>
+        <li>Les <strong>classifications de vente</strong> ne doivent pas être rendues obligatoires pour les factures, commandes et avoirs</li>
+        <li>L'<strong>axe analytique</strong> ne doit pas être rendu obligatoire pour les factures, avoirs, acomptes et commandes</li>
+        <li>Les <strong>taux de TVA</strong> doivent être harmonisés entre Evoliz et WooCommerce</li>
+    </ul>
+</div>
